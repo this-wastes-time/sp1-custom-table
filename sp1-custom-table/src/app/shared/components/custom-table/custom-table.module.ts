99dec97925be812f233a6aa3dce835a5ba25d4db
this-wastes-time <amacmusic90@gmail.com>
2025-01-27 16:02:07 -0600

    cosmetic updates

--- sp1-custom-table/src/app/shared/components/custom-table/custom-table.module.ts
+++ sp1-custom-table/src/app/shared/components/custom-table/custom-table.module.ts
@@ -8,17 +8,13 @@ import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { SearchBoxComponent } from '../search-box/search-box.component';
-import { MAT_FORM_FIELD_DEFAULT_OPTIONS, MatFormFieldDefaultOptions, MatFormFieldModule } from '@angular/material/form-field';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatOptionModule, provideNativeDateAdapter } from '@angular/material/core';
 import { MatSelectModule } from '@angular/material/select';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { GeneralSpinnerDirective } from '../../directives/general-spinner.directive';
-
-const formFieldConfig: MatFormFieldDefaultOptions = {
-  subscriptSizing: 'dynamic'
-};
 
 @NgModule({
   declarations: [],
@@ -61,7 +57,6 @@ const formFieldConfig: MatFormFieldDefaultOptions = {
     ReactiveFormsModule
   ],
   providers: [
-    { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: formFieldConfig },
     provideNativeDateAdapter(),
   ]
 })
